1ade2734d3a1716b7e1b1d83ed492c4c96e5f3cf
sunshinehubery <sunshinehubery@example.com>
2020-05-18 17:41:20 +0800

    实现图片上传

--- src/views/teacher/edit/index.vue
+++ src/views/teacher/edit/index.vue
@@ -1,120 +1,159 @@
 <template>
   <div class="app-container">
     <el-form label-width="120px">
-        <el-form-item label="讲师名称">
-            <el-input v-model="teacher.name"/>
-        </el-form-item>
-        <el-form-item label="讲师排序">
-            <el-input-number v-model="teacher.sort" controls-position="right" min="0"/>
-        </el-form-item>
-        <el-form-item label="讲师头衔">
-            <el-select v-model="teacher.level" clearable placeholder="请选择">
-            <!--
+      <el-form-item label="讲师名称">
+        <el-input v-model="teacher.name"/>
+      </el-form-item>
+      <el-form-item label="讲师排序">
+        <el-input-number v-model="teacher.sort" controls-position="right" min="0"/>
+      </el-form-item>
+      <el-form-item label="讲师头衔">
+        <el-select v-model="teacher.level" clearable placeholder="请选择">
+          <!--
                 数据类型一定要和取出的json中的一致，否则没法回填
                 因此，这里value使用动态绑定的值，保证其数据类型是number
             -->
-            <el-option :value="1" label="高级讲师"/>
-            <el-option :value="2" label="首席讲师"/>
-            </el-select>
-        </el-form-item>
-        <el-form-item label="讲师资历">
-            <el-input v-model="teacher.career"/>
-        </el-form-item>
-        <el-form-item label="讲师简介">
-            <el-input v-model="teacher.intro" :rows="10" type="textarea"/>
-        </el-form-item>
-        <!-- 讲师头像：TODO -->
-        <el-form-item>
-            <el-button :disabled="saveBtnDisabled" type="primary" @click="saveOrUpdate">保存</el-button>
-        </el-form-item>
+          <el-option :value="1" label="高级讲师"/>
+          <el-option :value="2" label="首席讲师"/>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="讲师资历">
+        <el-input v-model="teacher.career"/>
+      </el-form-item>
+      <el-form-item label="讲师简介">
+        <el-input v-model="teacher.intro" :rows="10" type="textarea"/>
+      </el-form-item>
+      <!-- 讲师头像：TODO -->
+      <el-form-item label="讲师头像">
+        <!-- 头衔缩略图 -->
+        <pan-thumb :image="teacher.avatar"/>
+        <!-- 文件上传按钮 -->
+        <el-button type="primary" icon="el-icon-upload" @click="imagecropperShow=true">更换头像</el-button>
+        <!--
+                v-show：是否显示上传组件
+                :key：类似于id，如果一个页面多个图片上传控件，可以做区分
+                :url：后台上传的url地址
+                @close：关闭上传组件
+                @crop-upload-success：上传成功后的回调 -->
+        <image-cropper
+          v-show="imagecropperShow"
+          :width="300"
+          :height="300"
+          :key="imagecropperKey"
+          :url="base_api"
+          field="file"
+          @close="close"
+          @crop-upload-success="cropSuccess"/>
+      </el-form-item>
+      <el-form-item>
+        <el-button :disabled="saveBtnDisabled" type="primary" @click="saveOrUpdate">保存</el-button>
+      </el-form-item>
     </el-form>
   </div>
 </template>
 <script>
 import teacher from '@/api/teacher'
-
+import ImageCropper from '@/components/ImageCropper'
+import PanThumb from '@/components/PanThumb'
 
 const defaultForm = {
-    name: '',
-    sort: 0,
-    level: '',
-    career: '',
-    intro: '',
-    avatar: ''
+  name: '',
+  sort: 0,
+  level: '',
+  career: '',
+  intro: '',
+  avatar: ''
 }
 export default {
-    data(){
-        return{
-            teacher: defaultForm,
-            saveBtnDisabled: false // 保存按钮是否禁用,
-        }
-    },
-    created(){
-        this.init()
-    },
-    methods: {
-        init(){
-            if(this.$route.params && this.$route.params.id){
-                console.log(id)
-                const id = this.$route.params.id
-                this.getTeacherById(id)
-            }
-        },
-        // 新增或修改
-        saveOrUpdate(){
-            this.saveBtnDisabled = true
-            if (!this.teacher.id) {
-                this.saveData()
-            } else {
-                this.updateTeacherById()
-            }
-        },
-        // 新增讲师
-        saveData(){
-            teacher.saveTeacher(this.teacher).then(response => {
-                return this.$message({
-                    type: 'success',
-                    message: '保存成功!'
-                })
-            }).then(resposne => {
-                this.$router.push({ path: '/teacher/list' })
-            }).catch((response) => {
-                // console.log(response)
-                this.$message({
-                    type: 'error',
-                    message: '保存失败'
-                })
-            })
-        },
-        // 根据id查询讲师信息
-        getTeacherById(id){
-            teacher.getTeacherById(id).then(response => {
-                this.teacher = response.data.data.item
-            }).catch((response) => {
-                this.$message({
-                    type: 'error',
-                    message: '获取数据失败'
-                })
-            })
-        },
-        // 根据id更新讲师信息
-        updateTeacherById(){
-            this.saveBtnDisabled = true
-            teacher.updateTeacherById(this.teacher).then(response => {
-                return this.$message({
-                    type: 'success',
-                    message: '修改成功!'
-                })
-            }).then(resposne => {
-                this.$router.push({ path: '/teacher/list' })
-            }).catch((response) => {
-                // console.log(response)
-                this.$message({
-                    type: 'error',
-                    message: '保存失败'
-                })
-            })
-        }
+  components: { ImageCropper, PanThumb },
+  data() {
+    return {
+      teacher: defaultForm,
+      saveBtnDisabled: false, // 保存按钮是否禁用
+      imagecropperShow: false, // 是否显示上传组件
+      imagecropperKey: 0, // 上传组件id
+      base_api: 'http://localhost:8083/oss/file/upload'
     }
+  },
+  created() {
+    this.init()
+  },
+  methods: {
+    init() {
+      if (this.$route.params && this.$route.params.id) {
+        console.log(id)
+        const id = this.$route.params.id
+        this.getTeacherById(id)
+      }
+    },
+    // 新增或修改
+    saveOrUpdate() {
+      this.saveBtnDisabled = true
+      if (!this.teacher.id) {
+        this.saveData()
+      } else {
+        this.updateTeacherById()
+      }
+    },
+    // 新增讲师
+    saveData() {
+      teacher.saveTeacher(this.teacher).then(response => {
+        return this.$message({
+          type: 'success',
+          message: '保存成功!'
+        })
+      }).then(resposne => {
+        this.$router.push({ path: '/teacher/list' })
+      }).catch((response) => {
+        // console.log(response)
+        this.$message({
+          type: 'error',
+          message: '保存失败'
+        })
+      })
+    },
+    // 根据id查询讲师信息
+    getTeacherById(id) {
+      teacher.getTeacherById(id).then(response => {
+        this.teacher = response.data.data.item
+      }).catch((response) => {
+        this.$message({
+          type: 'error',
+          message: '获取数据失败'
+        })
+      })
+    },
+    // 根据id更新讲师信息
+    updateTeacherById() {
+      this.saveBtnDisabled = true
+      teacher.updateTeacherById(this.teacher).then(response => {
+        return this.$message({
+          type: 'success',
+          message: '修改成功!'
+        })
+      }).then(resposne => {
+        this.$router.push({ path: '/teacher/list' })
+      }).catch((response) => {
+        // console.log(response)
+        this.$message({
+          type: 'error',
+          message: '保存失败'
+        })
+      })
+    },
+    // 上传成功后的回调函数
+    cropSuccess(data) {
+      this.imagecropperShow = false
+      this.teacher.avatar = data.url
+      // 上传成功后，重新打开上传组件时初始化组件，否则显示上一次的上传结果
+      this.imagecropperKey = this.imagecropperKey + 1
+    },
+    // 关闭上传组件
+    close() {
+      this.imagecropperShow = false
+      // 上传失败后，重新打开上传组件时初始化组件，否则显示上一次的上传结果
+      this.imagecropperKey = this.imagecropperKey + 1
+    }
+  }
 }
 </script>
